274a9b25f8bccd91b10f5779fbaf6a834fa09f9c
mrhasantanvir <smmhasantanvir@gmail.com>
2026-04-27 19:39:54 +1000

    Fix: Update PM2 port to 3060

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'tks-bd',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3001',
+      args: 'start -p 3060',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
